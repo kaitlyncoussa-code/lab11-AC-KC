e05fe026f8b46916fa2d174564b48666124659d8
MyDogIsCool4 <artattacc.art@gmail.com>
2025-11-19 17:36:45 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -21,8 +21,8 @@ def hypotenuse(a, b):
 
 def add(a, b):
     return a+b
-def sub(a,b):
-    return a-b
+def subtract(a, b):
+    return a - b
 def mul(a,b):
     return a*b
 def div(a,b):
@@ -31,42 +31,21 @@ def div(a,b):
     except ZeroDivisionError:
         print("error")
 
-def log(a,b):
+def logarithm(a, b):
     try:
-        return math.log(b,a)
+        math.log(b,a)# use math library/raise ValueError
     except ValueError:
-        print("error")
+        print("Whoopsies! Please try a valid input")
+
+
 def exp(a,b):
     return a^b
 
 
-def add(a, b):
-    a + b
 
-def subtract(a, b):
-    a - b
 
-def multiply(a, b):
-    a * b
 
-def divide(a, b):
-    if a ==0:
-        print("Whoopsies! Please enter a number that is not 0")
-        return
-    else:
-        b / a   # raise ZeroDivisionError if a == 0
 
-def logarithm(a, b):
-    if a<=0:
-        print("Whoopsies! Please enter a number that is greater than 0")
-        return
-    if b<=0:
-        print("Whoopsies! Please enter a number that is greater than 0")
-        return
-    math.log(b,a)# use math library/raise ValueError
-
-def exponent(a, b):
-    a**b
 
 
 
