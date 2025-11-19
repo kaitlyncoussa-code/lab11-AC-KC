813ec128bdb0ba10c2da750b39b464c7ba363c7c
kaitlyn <kaitlyncoussa@ufl.edu>
2025-11-19 17:20:00 -0500

    "modified calculator p1"

--- calculator.py
+++ calculator.py
@@ -5,7 +5,25 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
 def add(a, b): 
-    pass
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    try:
+       return b/a
+    except ZeroDivisionError:
+        print("error")
+
+def log(a,b):
+    try:
+        return math.log(b,a)
+    except ValueError:
+        print("error")
+def exp(a,b):
+    return a^b
 
 
