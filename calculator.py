6417746a9d23cf3cd0ac6d335b766b4471506b7b
MyDogIsCool4 <artattacc.art@gmail.com>
2025-11-19 17:07:23 -0500

    Testing

--- calculator.py
+++ calculator.py
@@ -5,7 +5,35 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    a + b
+
+def subtract(a, b):
+    a - b
+
+def multiply(a, b):
+    a * b
+
+def divide(a, b):
+    if a ==0:
+        print("Whoopsies! Please enter a number that is not 0")
+        return
+    else:
+        b / a   # raise ZeroDivisionError if a == 0
+
+def logarithm(a, b):
+    if a<=0:
+        print("Whoopsies! Please enter a number that is greater than 0")
+        return
+    if b<=0:
+        print("Whoopsies! Please enter a number that is greater than 0")
+        return
+    math.log(b,a)# use math library/raise ValueError
+
+def exponent(a, b):
+    a**b
+
 
 
